7fa1a32bc566f6ef9b12d5adb51cc52de1853cbb
Yassine Medah <yassinemedah1994@gmail.com>
2023-12-07 15:57:32 +0100

    axios product page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
+
+const API_URL = "http://localhost:5005";
 function Product() {
+  const [product, setProduct] = useState([]);
+  const getAllProducts = () => {
+    axios
+      .get(API_URL + "/api/product")
+      .then((response) => {
+        setProduct(response.data);
+        
+      })
+      .catch((error) => {
+        console.log("Error getting Products from the API...");
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    getAllProducts();
+  }, []);
   return (
-    <div>Product</div>
+    <>
+    {product.map((prdt, k) => {
+      return <h1>{prdt.name}</h1>
+    })}
+    
+   
+    </>
   )
 }
 
