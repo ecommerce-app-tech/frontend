def82d6b73558240660f5ef356d80faf822472f3
Yassine Medah <yassinemedah1994@gmail.com>
2023-12-13 12:46:32 +0100

    fix cart buh and design it

--- README.md
+++ README.md
@@ -7,80 +7,67 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-<div class="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
-<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
-  <div class="fixed inset-0 overflow-hidden">
-    <div class="absolute inset-0 overflow-hidden">
-      <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-      <div class="pointer-events-auto w-screen max-w-md">
-          <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-            <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-              <div class="flex items-start justify-between">
-                <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart</h2>
-                <div class="ml-3 flex h-7 items-center">
-                  <button type="button" class="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
-                    <span class="absolute -inset-0.5"></span>
-                    <span class="sr-only">Close panel</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                  </button>
+  <div className="flex-col flex items-center fixed inset-0 left-1/4 bg-white dark:bg-black gap-8  p-10  text-black dark:text-white font-normal uppercase text-sm">
+        <h1 className="text-2xl font-bold">Cart</h1>
+        <div className="absolute right-16 top-10">
+          <button
+            className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+            onClick={toggle}
+          >
+            Close
+          </button>
+        </div>
+        <div className="flex flex-col gap-4">
+          {cartItems.map((item) => (
+            <div className="flex justify-between items-center" key={item._id}>
+              <div className="flex gap-4">
+                <img src={item.image} alt={item.title} className="rounded-md h-24" />
+                <div className="flex flex-col">
+                  <h1 className="text-lg font-bold">{item.name}</h1>
+                  <p className="text-2xl font-semibold">{item.price}</p>
                 </div>
               </div>
-
-              <div class="mt-8">
-                <div class="flow-root">
-                  <ul role="list" class="-my-6 divide-y divide-gray-200">
-                    <li class="flex py-6">
-                      <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                        <img src={item.image} alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." class="h-full w-full object-cover object-center">
-                      </div>
-
-                        <div class="ml-4 flex flex-1 flex-col">
-                        <div>
-                          <div class="flex justify-between text-base font-medium text-gray-900">
-                            <h3>
-                              <a>{item.name}</a>
-                            </h3>
-                            <p class="ml-4">${item.price}</p>
-                          </div>
-                          <p class="mt-1 text-sm text-gray-500">{item.brand}</p>
-                        </div>
-                        <div class="flex flex-1 items-end justify-between text-sm">
-                          <p class="text-gray-500">item.quantity</p>
-
-                           <div class="flex">
-                            <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500" onClick={() => {
+              <div className="flex gap-4">
+                <button
+                  className='bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl'onClick={() => {
+                    addToCart(item)
+                  }}
+                >
+                  +
+                </button>
+                <p>{item.quantity}</p>
+                <button
+                  className='bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl' onClick={() => {
                     removeFromCart(item)
-                  }}>Remove</button>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
-                   
-
-                          <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
-              <div class="flex justify-between text-base font-medium text-gray-900">
-                <p>Subtotal</p>
-                <p>${getCartTotal()}</p>
-              </div>
-              <div class="mt-6">
-                <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>
-              </div>
-              <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
-                <p>
-                  or
-                  <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">
-                    Continue Shopping
-                    <span aria-hidden="true"> &rarr;</span>
-                  </button>
-                </p>
+                  }}
+                >
+                  -
+                </button>
               </div>
             </div>
-          </div>
+          ))}
         </div>
+        {
+          cartItems.length > 0 ? (
+            <div className="flex flex-col justify-between items-center">
+          <h1 className="text-lg font-bold">Total: ${getCartTotal()}</h1>
+          <button
+            className='bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full' onClick={() => {
+              clearCart()
+            }}
+          >
+            Clear cart
+          </button>
+         <br></br>
+         <Link to={`/checkout`}>
+          <button
+            className='bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full'>
+            Checkout
+            </button>
+            </Link>
+        </div>
+          ) : (
+            <h1 className="text-lg font-bold">Your cart is empty</h1>
+          )
+        }
       </div>
-    </div>
-  </div>
-</div>
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,87 +1,181 @@
-import PropTypes from 'prop-types'
-import { useContext,useState } from 'react'
-import { CartContext } from '../context/cart.jsx'
+import PropTypes from "prop-types";
+import { useContext, useState } from "react";
+import { CartContext } from "../context/cart.jsx";
 import { Link } from "react-router-dom";
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
-
- function Cart ({showModal, toggle}) {
-
-  const { cartItems, addToCart, removeFromCart, clearCart, getCartTotal } = useContext(CartContext)
-  
+function Cart({ showModal, toggle }) {
+  const [open, setOpen] = useState(true);
+  const { cartItems, addToCart, removeFromCart, clearCart, getCartTotal } =
+    useContext(CartContext);
 
   return (
     showModal && (
-      <div className="flex-col flex items-center fixed inset-0 left-1/4 bg-white dark:bg-black gap-8  p-10  text-black dark:text-white font-normal uppercase text-sm">
-        <h1 className="text-2xl font-bold">Cart</h1>
-        <div className="absolute right-16 top-10">
-          <button
-            className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            onClick={toggle}
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={toggle}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            Close
-          </button>
-        </div>
-        <div className="flex flex-col gap-4">
-          {cartItems.map((item) => (
-            <div className="flex justify-between items-center" key={item._id}>
-              <div className="flex gap-4">
-                <img src={item.image} alt={item.title} className="rounded-md h-24" />
-                <div className="flex flex-col">
-                  <h1 className="text-lg font-bold">{item.name}</h1>
-                  <p className="text-2xl font-semibold">{item.price}</p>
-                </div>
-              </div>
-              <div className="flex gap-4">
-                <button
-                  className='bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl'onClick={() => {
-                    addToCart(item)
-                  }}
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
                 >
-                  +
-                </button>
-                <p>{item.quantity}</p>
-                <button
-                  className='bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl' onClick={() => {
-                    removeFromCart(item)
-                  }}
-                >
-                  -
-                </button>
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                      <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-lg font-medium text-gray-900">
+                            Shopping cart
+                          </Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
+                              onClick={toggle}
+                            >
+                              <span className="absolute -inset-0.5" />
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </button>
+                          </div>
+                        </div>
+
+                        <div className="mt-8">
+                          <div className="flow-root">
+                            <ul
+                              role="list"
+                              className="-my-6 divide-y divide-gray-200"
+                            >
+                              {cartItems.map((item) => (
+                                <li key={item._id} className="flex py-6">
+                                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                    <img
+                                      src={item.image}
+                                      alt={item._id}
+                                      className="h-full w-full object-cover object-center"
+                                    />
+                                  </div>
+
+                                  <div className="ml-4 flex flex-1 flex-col">
+                                    <div>
+                                      <div className="flex justify-between text-base font-medium text-gray-900">
+                                        <h3>
+                                          <a href={item.href}>{item.name}</a>
+                                        </h3>
+                                        <p className="ml-4">${item.price}</p>
+                                      </div>
+                                      <p className="mt-1 text-sm text-gray-500">
+                                        {item.brand}
+                                      </p>
+                                    </div>
+                                    <div className="flex flex-1 items-end justify-between text-sm">
+                                      <button
+                                        className="bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl"
+                                        onClick={() => {
+                                          addToCart(item);
+                                        }}
+                                      >
+                                        +
+                                      </button>
+                                      <p>{item.quantity}</p>
+                                      <button
+                                        className="bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl"
+                                        onClick={() => {
+                                          removeFromCart(item);
+                                        }}
+                                      >
+                                        -
+                                      </button>
+                                    </div>
+                                  </div>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex items-center justify-center px-4 py-3 sm:px-6">
+                        <button
+                          type="button"
+                          className="font-medium text-indigo-600 hover:text-indigo-500"
+                          onClick={() => {
+                            clearCart();
+                          }}
+                        >
+                         Clear Cart
+                        </button>
+                      </div>
+                      <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                        <div className="flex justify-left text-base font-medium text-gray-900">
+                          <p>Total : </p>
+                          <p>${getCartTotal()}</p>
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">
+                          Shipping and taxes Included.
+                        </p>
+                        <div className="mt-6">
+                        <Link to={`/checkout`}>
+                          <a
+                            href="#"
+                            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                            onClick={toggle}
+
+                          >
+                            Checkout
+                          </a>
+                          </Link>
+                        </div>
+                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                          <p>
+                            or
+                            <button
+                              type="button"
+                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                              onClick={toggle}
+                            >
+                              Continue Shopping
+                              <span aria-hidden="true"> &rarr;</span>
+                            </button>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
               </div>
             </div>
-          ))}
-        </div>
-        {
-          cartItems.length > 0 ? (
-            <div className="flex flex-col justify-between items-center">
-          <h1 className="text-lg font-bold">Total: ${getCartTotal()}</h1>
-          <button
-            className='bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full' onClick={() => {
-              clearCart()
-            }}
-          >
-            Clear cart
-          </button>
-         <br></br>
-         <Link to={`/checkout`}>
-          <button
-            className='bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full'>
-            Checkout
-            </button>
-            </Link>
-        </div>
-          ) : (
-            <h1 className="text-lg font-bold">Your cart is empty</h1>
-          )
-        }
-      </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     )
-  )
+  );
 }
 
 Cart.propTypes = {
   showModal: PropTypes.bool,
-  toggle: PropTypes.func
-}
+  toggle: PropTypes.func,
+};
 
-export default Cart
+export default Cart;
